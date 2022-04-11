66d4eb679acf32e9f6e822bfa76bc153db8a2983
NickNameInvalid <zssmh98@outlook.com>
2022-04-11 19:35:13 -0400

    Update c_6.sql

--- queries/c_6.sql
+++ queries/c_6.sql
@@ -1,15 +1,15 @@
 select qid, title, q_body
 from Questions
-where title like '%how to%'
+where stid = 14 and title like '%how to%'
 
 union
 
 select qid, title, q_body
 from Questions
-where q_body like '%how to%'
+where stid = 14 and q_body like '%how to%'
 
 union 
 
 select Questions.qid, title, Questions.q_body
 from Questions natural join Answers
-where Answers.a_body like '%how to%'
+where stid = 14 and Answers.a_body like '%how to%'
