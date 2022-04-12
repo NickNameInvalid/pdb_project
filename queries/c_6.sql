6d1b553ad70188c2fb48fceafd8b202a96eb3511
NickNameInvalid <zssmh98@outlook.com>
2022-04-11 22:13:11 -0400

    Update c_6.sql

--- queries/c_6.sql
+++ queries/c_6.sql
@@ -1,17 +1,17 @@
 select qid, title, q_body from(
-select qid, q_username as username, title, q_body, post_time as timestamp
+select qid, q_username as username, title, q_body, post_time as timestamp, 0.25 as weight
 from Questions
-where stid = 14 and title like '%how to%'
+where stid = 14 and title like '%e%'
 
-union
+union all
 
-select qid, q_username as username, title, q_body, post_time as timestamp
+select qid, q_username as username, title, q_body, post_time as timestamp, 0.55 as weight
 from Questions
-where stid = 14 and q_body like '%how to%'
+where stid = 14 and q_body like '%e%'
 
-union 
+union all
 
-select qid, q_username as username, title, q_body, answer_time as timestamp
+select qid, q_username as username, title, q_body, answer_time as timestamp, 0.2 as weight
 from Questions natural join Answers
-where stid = 14 and a_body like '%how to%'
-) as q natural join users order by timestamp, karma_points desc
+where stid = 14 and a_body like '%e%'
+) as q group by qid, title, q_body, timestamp order by sum(weight), timestamp desc
