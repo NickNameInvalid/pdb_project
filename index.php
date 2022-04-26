5508c411470e48682916037237e3855ce0a27448
NickNameInvalid <zssmh98@outlook.com>
2022-04-26 17:24:45 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,19 @@
 </head>
 <body>
 <h1>Welcome to the forum!</h1>
-<?php
+<div id="g_topic_receiver"></div>
 
+<?php
+include('application/mysqlidb.php');
+$mysqli = establish_conn();
+$g_topic_query = $mysqli->prepare("select gtname from generaltopics");
+$g_topic_query -> execute();
+$g_topic_query->bind_result($gtname);
+while ($g_topic_query->fetch()) {
+    echo "<h3>$gtname</h3>";
+}
+
+$mysqli->close();
 ?>
 </body>
 
