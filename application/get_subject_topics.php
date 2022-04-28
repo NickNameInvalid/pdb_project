0c273a91ebb4ae9cdb8c89b990a4ef97534c3784
NickNameInvalid <zssmh98@outlook.com>
2022-04-28 19:58:22 -0400

    update question posting

---
+++ application/get_subject_topics.php
@@ -0,0 +1,18 @@
+<?php
+include('mysqlidb.php');
+$mysqli = establish_conn();
+$gtid = number_format($_POST['gtid']);
+$stmt = $mysqli->prepare("select concat('stid_', stid) as stid, stname from subjecttopics where gtid = ?");
+$stmt->bind_param("i", $gtid);
+$stmt -> execute();
+$stmt->bind_result($stid, $stname);
+echo "<option selected>Subject Topics</option>";
+while ($stmt->fetch())
+{
+    echo <<<heredoc
+    <option id="$stid">$stname</option>
+    heredoc;
+}
+$stmt->close();
+$mysqli->close();
+?>
