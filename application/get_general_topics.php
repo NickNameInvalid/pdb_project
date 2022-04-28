9cd9f0186b083992a7383151dd892936ec82164c
NickNameInvalid <zssmh98@outlook.com>
2022-04-28 19:17:27 -0400

    update

---
+++ application/get_general_topics.php
@@ -0,0 +1,17 @@
+<?php
+include('mysqlidb.php');
+$mysqli = establish_conn();
+
+$stmt = $mysqli->prepare("select concat('gtid_', gtid) as gtid, gtname from generaltopics");
+$stmt -> execute();
+$stmt->bind_result($gtid, $gtname);
+echo "<option selected>General Topics</option>";
+while ($stmt->fetch())
+{
+    echo <<<heredoc
+        <option id="$gtid">$gtname</option>
+    heredoc;
+}
+$stmt->close();
+$mysqli->close();
+?>
