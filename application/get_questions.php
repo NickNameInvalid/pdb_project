efb4a75ff05016eb600be217fd5895b52918e6cf
NickNameInvalid <zssmh98@outlook.com>
2022-04-28 16:26:19 -0400

    update

---
+++ application/get_questions.php
@@ -0,0 +1,28 @@
+<?php
+include('mysqlidb.php');
+$mysqli = establish_conn();
+
+$stmt = $mysqli->prepare("select q_username, concat(gtname, ' / ', stname) as topics , title, q_body, post_time, status from questions natural join subjecttopics natural join generaltopics where q_visible_status = 1");
+$stmt -> execute();
+$stmt->bind_result($u, $tc, $tt, $bd, $pt, $st);
+while($stmt->fetch())
+{
+    echo <<<heredoc
+        <div class="card mb-3">
+            <div class="card-body">
+                <h4 class="card-title">$tt</h4>
+                <div class="dropdown-divider"></div>
+                <p class="card-text">$bd</p>
+                <div class="post_time d-flex gap-lg-5">
+                    <div class="p-2 user_post">$u</div>
+                    <div class="p-2 time_post">$pt</div>
+                    <div class="p-2 post_topics">$tc</div>
+                    <div class="p-2 question_status">$st</div>
+                </div>
+            </div>
+        </div>
+    heredoc;
+}
+$stmt->close();
+$mysqli->close();
+?>
