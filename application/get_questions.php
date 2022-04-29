f8cb48bf835ed6650eefb61156709c9d6a35d3ba
NickNameInvalid <zssmh98@outlook.com>
2022-04-29 15:32:08 -0400

    add framework for query

--- application/get_questions.php
+++ application/get_questions.php
@@ -2,14 +2,30 @@
 include('mysqlidb.php');
 session_start();
 $mysqli = establish_conn();
-
-$stmt = $mysqli->prepare("select q_username, concat(gtname, ' / ', stname) as topics , title, q_body, post_time, status from questions natural join subjecttopics natural join generaltopics where q_visible_status = 1");
+$gtid = $_GET['gtid'] ?? "dft";
+$stid = $_GET['stid'] ?? "dft";
+$keywd = $_GET['key'] ?? "";
+$stype = $_GET['s_stype'] ?? "dft";
+$sql_text = "select qid, q_username, concat(gtname, ' / ', stname) as topics , title, q_body, post_time, status 
+                from questions natural join subjecttopics natural join generaltopics 
+                where q_visible_status = 1";
+if($stid != "dft")
+{
+    $stid_num = number_format($stid);
+    $sql_text .= "and stid = ?";
+}
+else if ($gtid != "dft")
+{
+    $gtid_num = number_format($gtid);
+    $sql_text .= "and gtid = ?";
+}
+$stmt = $mysqli->prepare($sql_text);
 $stmt -> execute();
-$stmt->bind_result($u, $tc, $tt, $bd, $pt, $st);
+$stmt->bind_result($qid, $u, $tc, $tt, $bd, $pt, $st);
 while($stmt->fetch())
 {
     echo <<<heredoc
-        <div class="card mb-3">
+        <div class="card mb-3" id="qid_$qid">
             <div class="card-body">
                 <h4 class="card-title">$tt</h4>
                 <div class="dropdown-divider"></div>
