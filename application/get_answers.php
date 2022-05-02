d74906b0acd7b361c4a90a5e8ff38935d943cf1c
NickNameInvalid <zssmh98@outlook.com>
2022-05-02 18:29:31 -0400

    update interface for question detail

--- application/get_answers.php
+++ application/get_answers.php
@@ -15,6 +15,7 @@ $sql = "select aid, a_username, a_body, thumb_ups, answer_time, best_answer
         where qid = ? and a_visible_status = 1
         order by answer_time desc";
 
+echo "<h4 class='mb-2'>Answers</h4>";
 if ($stmt = $mysqli->prepare($sql)) {
     $stmt->bind_param("i", $qid);
     $stmt->execute();
@@ -34,15 +35,18 @@ if ($stmt = $mysqli->prepare($sql)) {
                     <div class="post_time d-flex gap-lg-5">
                         <div class="p-2 user_post">$a_user</div>
                         <div class="p-2 time_post">$a_time</div>
-                        <button type="button" class="btn btn-default" data-bs-toggle="modal" data-bs-target="#post_question_model" title="Post a new question" id="post_float_btn" style="height: 40px;vertical-align:middle;">
+                        <button type="button" class="btn btn-default" id="like_$aid" style="height: 40px;vertical-align:middle;">
                             <div class="d-flex gap-1">
                                 <img src="../framework/bootstrap-icons-1.8.1/hand-thumbs-up.svg" alt="like number">
                                 <div class="p-0">$thumb_ups</div>
                             </div>
                         </button>
-                        <button type="button" class="btn btn-default" data-bs-toggle="modal" data-bs-target="#post_question_model" title="Post a new question" id="post_float_btn" style="height: 40px;vertical-align:middle;">
+                        <button type="button" class="btn btn-default" id="best_$aid" style="height: 40px;vertical-align:middle;">
                                 <img src="../framework/bootstrap-icons-1.8.1/trophy.svg" alt="best answer status">
                                 <div class="p-0 best_tip" hidden></div>
+                        </button>
+                        <button type="button" class="btn btn-default" id="edit_$aid" style="height: 40px;vertical-align:middle;">
+                            <img src="../framework/bootstrap-icons-1.8.1/pencil-square.svg" alt="edit answer">
                         </button>
                     </div>
                 </div>
