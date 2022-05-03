b4c9169cb3534956152d78aa5797b53a4d3ba3a2
NickNameInvalid <zssmh98@outlook.com>
2022-05-03 15:25:25 -0400

    update xss fix

--- templates/js/utils.js
+++ templates/js/utils.js
@@ -4,3 +4,11 @@ function GetQueryString(name)
     var r = window.location.search.substr(1).match(reg);
     if(r!=null)return  unescape(r[2]); return null;
 }
+
+function HTMLEncode(html) {
+    var temp = document.createElement("div");
+    (temp.textContent != null) ? (temp.textContent = html) : (temp.innerText = html);
+    var output = temp.innerHTML;
+    temp = null;
+    return output;
+}
