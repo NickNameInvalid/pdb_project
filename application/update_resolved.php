b1956c15b7f8d7a6226280dd003a3cc0b219777e
YiLight <34399162+YiLight@users.noreply.github.com>
2022-05-03 00:39:14 -0400

    Create update_resolved.php

---
+++ application/update_resolved.php
@@ -0,0 +1,47 @@
+<?php
+include('mysqlidb.php');
+session_start();
+$mysqli = establish_conn();
+$myupdate = establish_conn();
+$qid = $_POST['qid'];
+$p_user = $_POST['p_user'];
+$c_user = $_SESSION['username'] ?? "dft";
+
+if ($c_user == "dft") {
+    echo "<script>alert('You have not logged in!')</script>";
+    return;
+}
+
+if ($c_user != $p_user) {
+    echo "<script>alert('You cannot set resolved because you are not the owner of this question!')</script>";
+    return;
+}
+
+$sql = "select status 
+        from Questions
+        where qid = ?";
+
+$stmt = $mysqli->prepare($sql);
+$stmt->bind_param("i", $qid);
+$stmt->execute();
+$stmt->bind_result($status);
+$stmt->fetch();
+
+if ($status == "unsolved") {
+    $sql = "update Question 
+            set status = 'resolved'
+            where qid = ?";
+} else {
+    $sql = "update Question 
+            set status = 'unsolved'
+            where qid = ?";
+}
+
+$update = $myupdate->prepare($sql);
+$update->bind_param("i", $qid);
+$update->execute();
+
+$update->close();
+$stmt->close();
+$myupdate->close();
+$mysqli->close();
