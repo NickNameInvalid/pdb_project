41a58322cf7301e70a96e2bfeb9828035445768a
NickNameInvalid <zssmh98@outlook.com>
2022-05-03 13:41:31 -0400

    Update update_thumb_ups.php

--- application/update_thumb_ups.php
+++ application/update_thumb_ups.php
@@ -19,9 +19,10 @@ $stmt = $mysqli->prepare($sql);
 $stmt->bind_param("si", $user, $aid);
 $stmt->execute();
 $stmt->bind_result($like_status);
+$stmt->store_result();
 $stmt->fetch();
 
-if ($like_status == null) {
+if ($stmt->num_rows == 0) {
     $sql_insert = "insert into Likes (username, aid) values (?, ?)";
 
     $update = $myupdate->prepare($sql_insert);
@@ -35,7 +36,7 @@ if ($like_status == null) {
     $update = $myupdate->prepare($sql_update_0);
     $update->bind_param("si", $user, $aid);
     $update->execute();
-} else {
+} else if ($like_status == 0){
     $sql_update_1 = "update Likes
             set like_status = 1
             where username = ? and aid = ?";
