3a3278866a504c38491050b0ab35a104fea68d3e
NickNameInvalid <zssmh98@outlook.com>
2022-05-04 13:09:39 -0400

    Update update_thumb_ups.php

--- application/update_thumb_ups.php
+++ application/update_thumb_ups.php
@@ -29,37 +29,55 @@ if ($stmt->num_rows == 0) {
     $update->bind_param("si", $user, $aid);
     $update->execute();
 } else if ($like_status == 1) {
-    $sql_lock_0 = "select * from likes where username = ? and aid = ? for update";
-
-    $sql_update_0 = "update Likes
+    $mysqli->query("drop procedure if exists update_likes_0");
+    $mysqli->query("create procedure 
+            update_likes_0(in uname varchar(45), in id int) 
+            begin 
+            select * from likes where username = uname and aid = id; 
+            update Likes
             set like_status = 0
-            where username = ? and aid = ?";
-
-    $lock_0 = $myupdate->prepare($sql_lock_0);
-    $lock_0->bind_param("si", $user, $aid);
-    $lock_0->execute();
-    $lock_0->close();
-
-    $update = $myupdate->prepare($sql_update_0);
-    $update->bind_param("si", $user, $aid);
-    $update->execute();
+            where username = uname and aid = id; end;");
+    $update_stmt = $mysqli->prepare("call update_likes_0(?, ?)");
+    $update_stmt->bind_param("si", $user, $aid);
+    $update_stmt->execute();
+    $update_stmt->close();
+//    $sql_update_0 = "update Likes
+//            set like_status = 0
+//            where username = ? and aid = ?";
+//
+//    $update = $myupdate->prepare($sql_update_0);
+//    $update->bind_param("si", $user, $aid);
+//    $update->execute();
 } else if ($like_status == 0){
-    $sql_lock_1 = "select * from likes where username = ? and aid = ? for update";
-    $sql_update_1 = "update Likes
+//    $sql_lock_1 = "select * from likes where username = ? and aid = ? for update";
+//    $sql_update_1 = "update Likes
+//            set like_status = 1
+//            where username = ? and aid = ?";
+
+//    $lock_1 = $myupdate->prepare($sql_lock_1);
+//    $lock_1->bind_param("si", $user, $aid);
+//    $lock_1->execute();
+//    $lock_1->close();
+//
+//    $update = $myupdate->prepare($sql_update_1);
+//    $update->bind_param("si", $user, $aid);
+//    $update->execute();
+
+    $mysqli->query("drop procedure if exists update_likes_1");
+    $mysqli->query("create procedure 
+            update_likes_1(in uname varchar(45), in id int) 
+            begin 
+            select * from likes where username = uname and aid = id; 
+            update Likes
             set like_status = 1
-            where username = ? and aid = ?";
-
-    $lock_1 = $myupdate->prepare($sql_lock_1);
-    $lock_1->bind_param("si", $user, $aid);
-    $lock_1->execute();
-    $lock_1->close();
-
-    $update = $myupdate->prepare($sql_update_1);
-    $update->bind_param("si", $user, $aid);
-    $update->execute();
+            where username = uname and aid = id; end;");
+    $update_stmt = $mysqli->prepare("call update_likes_1(?, ?)");
+    $update_stmt->bind_param("si", $user, $aid);
+    $update_stmt->execute();
+    $update_stmt->close();
 }
 
 $stmt->close();
-$update->close();
+//$update->close();
 $mysqli->close();
 $myupdate->close();
