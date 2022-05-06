f9532b228576ff4edcfc7c166e8a55f443cabd97
NickNameInvalid <zssmh98@outlook.com>
2022-05-05 21:55:08 -0400

    update

--- application/get_cur_question.php
+++ application/get_cur_question.php
@@ -5,25 +5,28 @@ $mysqli = establish_conn();
 $qid = $_GET['qid'] ?? "";
 $user = $_SESSION['username'] ?? "";
 
-$sql = "select qid, q_username, concat(gtname, ' / ', stname) as topics , title, q_body, post_time, status 
+$sql = "select stid, gtid, qid, q_username, gtname, stname, title, q_body, post_time, status 
         from questions natural join subjecttopics natural join generaltopics 
         where qid = ? and q_visible_status = 1";
 
 if ($stmt = $mysqli->prepare($sql)) {
     $stmt->bind_param("i", $qid);
     $stmt->execute();
-    $stmt->bind_result($qid, $q_user, $q_topics, $q_title, $q_body, $q_time, $q_status);
+    $stmt->bind_result($stid, $gtid, $qid, $q_user, $gtname, $stname, $q_title, $q_body, $q_time, $q_status);
     $stmt->fetch();
 
     $res = Array();
     $res['editable'] = $user == $q_user;
     $res['qid'] = $qid;
     $res['q_user'] = $q_user;
-    $res['q_topics'] = $q_topics;
+    $res['st_topics'] = $stname;
+    $res['gt_topics'] = $gtname;
     $res['q_title'] = $q_title;
     $res['q_body'] = $q_body;
     $res['q_time'] = $q_time;
     $res['q_status'] = $q_status;
+    $res['s_tid'] = $stid;
+    $res['g_tid'] = $gtid;
 
 
     $stmt->close();
